ea2daca2ef45c909b439f850bfb041025b35bad2
martinhoecherl <108944475+martinhoecherl@users.noreply.github.com>
2022-07-11 15:13:02 +0200

    Update test.py

--- test.py
+++ test.py
@@ -8,20 +8,21 @@ uploaded_file = st.file_uploader('Select files', accept_multiple_files=True, typ
 
 if str(uploaded_file) != '[]':
 
-    df = np.zeros((len(uploaded_file), 2)).astype(object)
+    df = np.zeros((len(uploaded_file), 3)).astype(object)
 
     for i in range(len(uploaded_file)):
 
+        df[i, 0] = uploaded_file[i].name
         pdf_read = extract_text(uploaded_file[i])
 
         if str(re.search(rf"(Dieselverbrauch: )", pdf_read)) != 'None' and re.findall('[0-9]+', pdf_read[re.search(rf"(Dieselverbrauch: )", pdf_read).end():re.search(rf"(Dieselverbrauch: )", pdf_read).end() + 5]) != re.findall('[0-9]+', pdf_read[re.search(rf"(kum. Dieselverbrauch: )", pdf_read).end():re.search(rf"(kum. Dieselverbrauch: )", pdf_read).end()+6]):
             y = re.search(rf"(Dieselverbrauch: )", pdf_read)
             y_num = re.findall('[0-9]+', pdf_read[y.end(): y.end() + 5])
-            df[i, 0] = float(y_num[0])
+            df[i, 1] = float(y_num[0])
         if str(re.search(rf"(kum. Dieselverbrauch: )", pdf_read)) != 'None':
             x = re.search(rf"(kum. Dieselverbrauch: )", pdf_read)
             x_num = re.findall('[0-9]+', pdf_read[x.end(): x.end()+6])
-            df[i, 1] = float(x_num[0])
+            df[i, 2] = float(x_num[0])
 
     df = pd.DataFrame(df, columns=['Diesel consumption', 'cumulative Diesel consumption'])
 
